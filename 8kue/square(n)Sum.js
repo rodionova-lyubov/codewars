0225aae12851f16c0ee3e10430f24a80970504fc
Lyubov Rodionova <rodionovaxxi@gmail.com>
2021-06-02 16:50:32 +0300

    tab to space

--- 8kue/square(n)Sum.js
+++ 8kue/square(n)Sum.js
@@ -8,11 +8,11 @@ For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9
 */
 
 function squareSum(numbers) {
-	let sum = 0;
-	for (let number of numbers) {
-		sum += number ** 2;
+   let sum = 0;
+   for (let number of numbers) {
+      sum += number ** 2;
 	}
-	return sum;
+   return sum;
 }
 
 
