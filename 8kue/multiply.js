1e32014856dd338df0b03957b35fd70fc202c2e4
Lyubov Rodionova <rodionovaxxi@gmail.com>
2021-06-02 14:36:03 +0300

    add jadebCasingStrings.js

--- 8kue/multiply.js
+++ 8kue/multiply.js
@@ -1,5 +1,5 @@
 /*
-https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+https://www.codewars.com/kata/50654ddff44f800200000004
 
 This code does not execute properly. Try to figure out why.
 
