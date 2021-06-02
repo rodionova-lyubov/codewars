1befd2baef9e283a79a2db9fc9fdaaa43f0971d4
Lyubov Rodionova <rodionovaxxi@gmail.com>
2021-06-02 15:19:31 +0300

    add best practices

--- 8kue/square(n)Sum.js
+++ 8kue/square(n)Sum.js
@@ -6,6 +6,7 @@ Complete the square sum function so that it squares each number passed into it a
 For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9
 
 */
+
 function squareSum(numbers) {
 	let sum = 0;
 	for (let number of numbers) {
@@ -13,3 +14,13 @@ function squareSum(numbers) {
 	}
 	return sum;
 }
+
+
+//best practices
+/*
+function squareSum(numbers){
+  return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
+}
+*/
