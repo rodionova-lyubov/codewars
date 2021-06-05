50dd516a51716654788afec5027b6231f5ac9c05
Lyubov Rodionova <rodionovaxxi@gmail.com>
2021-06-05 17:49:40 +0300

    add returnToSanity.js

---
+++ 8kue/returnToSanity.js
@@ -0,0 +1,13 @@
+/*
+https://www.codewars.com/kata/514a7ac1a33775cbb500001e
+
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+*/
+
+function mystery() {
+   var results = {
+      sanity: 'Hello'
+   };
+   return results;
+}
